d83a3ff450a14e0bab32af5254242f26ae11de14
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-23 21:38:43 +0300

    Create 16PrintWordsFromAAAtoZZZ.cpp

---
+++ Problems/16PrintWordsFromAAAtoZZZ.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+void PrintWordsFromAAAtoZZZ()
+{
+	cout << "\n";
+	string word = "";
+
+	for (int i = 65; i <= 90; i++)
+	{
+		for (int j = 65; j <= 90; j++)
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				word = word + char(i);
+				word = word + char(j);
+				word = word + char(k);
+
+				cout << word << endl;
+
+				word = "";
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	PrintWordsFromAAAtoZZZ();
+	
+	return 0;
+}
