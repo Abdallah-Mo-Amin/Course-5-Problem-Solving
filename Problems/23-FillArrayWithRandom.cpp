c34e0abaad3258d812a321eedb7b613333c8714a
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-23 21:51:24 +0300

    Create 23-FillArrayWithRandom.cpp

---
+++ Problems/23-FillArrayWithRandom.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int GenrateRandomNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return RandNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int &arrLength)
+{
+	cout << "Enter Numver of elements\n";
+	cin >> arrLength;
+
+	cout << "\nFilling the array with random numbers\n";
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = GenrateRandomNumber(1, 100);
+	}
+	cout << endl;
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrlength;
+
+	FillArrayWithRandomNumbers(arr, arrlength);
+
+	cout << "Array Elements: ";
+	PrintArray(arr, arrlength);
+
+	return 0;
+}
