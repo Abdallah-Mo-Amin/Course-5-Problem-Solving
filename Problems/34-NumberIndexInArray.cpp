706d5e9916ab1eac695ca9821f10613cf6537975
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-26 00:23:46 +0300

    Create 34-NumberIndexInArray.cpp

---
+++ Problems/34-NumberIndexInArray.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int ReadPositiveNumber(string Massage)
+{
+	int Num = 0;
+	do
+	{
+		cout << Massage << endl;
+		cin >> Num;
+	} while (Num < 0);
+	return Num;
+}
+
+int RandomNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return RandNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+short FindNumberPoaitionInArray(int arr[100], int arrLength, int NumberToSearch)
+{
+	cout << "Number you are looking for is: " << NumberToSearch;
+	for (int i = 0; i < arrLength; i++)
+	{
+		if (arr[i] == NumberToSearch)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << "Element [" << i << "] : " << arr[i] << endl;
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	// seeds the random number generator in c++, called only once
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+
+	int arrlength = ReadPositiveNumber("Please Enter how many elements in the array? ");
+	
+	FillArrayWithRandomNumbers(arr, arrlength);
+	cout << "\nArray 1 Elements:\n";
+	PrintArray(arr, arrlength);
+
+	int NumberToSerchFor = ReadPositiveNumber("enter a number to search for? ");
+	
+	short NumberPosition = FindNumberPoaitionInArray(arr, arrlength, NumberToSerchFor);
+	if (NumberPosition == -1)
+	{
+		cout << "\nThe Number is not found:-(\n";
+	}
+	else
+	{
+		cout << "\nThe number found at posititon: " << NumberPosition
+			<< "\nThe number found it's order: " << NumberPosition + 1 << "\n";
+	}
+	
+	return 0;
+}
