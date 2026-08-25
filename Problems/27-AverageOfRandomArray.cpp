edc371caab5c0d234175ffe3b4300bd54fd00b7f
Abdallah-Mo-Amin <abdallahamin567@gmail.com>
2026-08-26 00:14:44 +0300

    Create 27-AverageOfRandomArray.cpp

---
+++ Problems/27-AverageOfRandomArray.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int GenrateRandomNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return RandNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int &arrLength)
+{
+	cout << "Enter Numver of elements\n";
+	cin >> arrLength;
+
+	cout << "\nFilling the array with random numbers\n";
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = GenrateRandomNumber(1, 100);
+	}
+	cout << endl;
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int SumNumberInTheArray(int arr[100], int arrLength)
+{
+	int Sum = 0;
+
+	for (int i = 0; i < arrLength; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+
+float AverageNumberInTheArray(int arr[100], int arrLength)
+{
+	return (float) SumNumberInTheArray(arr, arrLength) / arrLength;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrlength;
+
+	FillArrayWithRandomNumbers(arr, arrlength);
+
+	cout << "Array Elements: ";
+	PrintArray(arr, arrlength);
+
+
+	cout << "\n array average is: " << AverageNumberInTheArray(arr, arrlength) << endl;
+
+	return 0;
+}
